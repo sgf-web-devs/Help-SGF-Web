3e7cf81ebd4f8772fe46154456cdfa5b23c7696d
Eric McCullough <eric96@live.missouristate.edu>
2019-11-10 14:21:02 -0600

    Initial commit for Categories page

---
+++ Pages/Categories.cshtml.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using HelpSGFWebApp.Models;
+using HelpSGFWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelpSGFWebApp.Pages 
+{
+    public class Categories : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+} 
